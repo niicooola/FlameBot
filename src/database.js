e1f45402226feb384975461397f2f961dd5a7da2
niicooola <skitishmc@gmail.com>
2026-06-03 21:12:46 -0400

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
 
+async function connectDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log('Mongo connected');
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = {
+    connectDatabase
+};
