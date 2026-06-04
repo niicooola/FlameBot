33cf2c998d78bea2e64af2ea4236f97d6139646c
niicooola <skitishmc@gmail.com>
2026-06-03 21:15:50 -0400

    Update guildMemberAdd.js

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,1 +1,13 @@
+const { PREFIX } = require('../config');
 
+module.exports = function(client) {
+    client.on('guildMemberAdd', async member => {
+        try {
+            await member.send(
+                `Welcome to **${member.guild.name}**!\nType \`${PREFIX}help\` in the server.`
+            );
+        } catch {
+            console.log(`Could not DM ${member.user.tag}`);
+        }
+    });
+};
