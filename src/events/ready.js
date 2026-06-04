4481a5bf14a2a16288fd5a44cefee384ec05fdad
niicooola <skitishmc@gmail.com>
2026-06-03 21:15:26 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,8 @@
+const { startMarketLoop } = require('../systems/market');
 
+module.exports = function(client) {
+    client.once('ready', () => {
+        console.log(`🔥 Logged in as ${client.user.tag}`);
+        startMarketLoop(client);
+    });
+};
