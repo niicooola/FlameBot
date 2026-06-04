aee74a44a8bc22c6f89d2f3cf76d95677c2024d6
niicooola <skitishmc@gmail.com>
2026-06-03 21:14:06 -0400

    Update amounts.js

--- src/utils/amounts.js
+++ src/utils/amounts.js
@@ -1,1 +1,20 @@
+function cleanAmount(value) {
+    const n = parseInt(value);
 
+    return Number.isFinite(n)
+        ? n
+        : null;
+}
+
+function cleanFloat(value) {
+    const f = parseFloat(value);
+
+    return Number.isFinite(f)
+        ? f
+        : null;
+}
+
+module.exports = {
+    cleanAmount,
+    cleanFloat
+};
