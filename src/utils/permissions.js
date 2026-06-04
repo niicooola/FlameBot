67de404995bc4d2847de28814d0e7b06b839debc
niicooola <skitishmc@gmail.com>
2026-06-03 21:14:31 -0400

    Update permissions.js

--- src/utils/permissions.js
+++ src/utils/permissions.js
@@ -1,1 +1,27 @@
+const {
+    PermissionsBitField
+} = require('discord.js');
 
+function isStaff(member) {
+    return member.permissions.has(
+        PermissionsBitField.Flags.Administrator
+    );
+}
+
+function isMod(member) {
+    return member.permissions.has(
+        PermissionsBitField.Flags.Administrator
+    );
+}
+
+function isAdmin(member) {
+    return member.permissions.has(
+        PermissionsBitField.Flags.Administrator
+    );
+}
+
+module.exports = {
+    isStaff,
+    isMod,
+    isAdmin
+};
