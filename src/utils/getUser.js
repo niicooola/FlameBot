903c36bc91fb252830e2f733bcadf907fe7a7b03
niicooola <skitishmc@gmail.com>
2026-06-03 21:13:50 -0400

    Update getUser.js

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -1,1 +1,19 @@
+const User = require('../models/User');
 
+async function getUser(id) {
+    let user = await User.findOne({
+        id
+    });
+
+    if (!user) {
+        user = await User.create({
+            id
+        });
+    }
+
+    return user;
+}
+
+module.exports = {
+    getUser
+};
