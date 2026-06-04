56d64cec28cccf7d8f121c726b7b7cbde7206e19
niicooola <skitishmc@gmail.com>
2026-06-03 21:30:04 -0400

    Update getUser.js

--- src/utils/getUser.js
+++ src/utils/getUser.js
@@ -1,19 +1,23 @@
 const User = require('../models/User');
 
 async function getUser(id) {
-    let user = await User.findOne({
-        id
-    });
+    let user = await User.findOne({ id });
 
     if (!user) {
-        user = await User.create({
-            id
-        });
+        try {
+            user = await User.create({ id });
+        } catch {
+            user = await User.findOne({ id });
+        }
     }
+
+    if (!user.portfolios) user.portfolios = new Map();
+    if (!user.inventory) user.inventory = [];
+    if (!user.badges) user.badges = [];
+    if (!user.notes) user.notes = [];
+    if (!user.todos) user.todos = [];
 
     return user;
 }
 
-module.exports = {
-    getUser
-};
+module.exports = { getUser };
