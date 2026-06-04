f72ba0f56de9d30ca7ca0b871f8541d115067c2b
niicooola <skitishmc@gmail.com>
2026-06-03 21:29:44 -0400

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,52 +1,23 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    id: {
-        type: String,
-        unique: true,
-        required: true
-    },
+    id: { type: String, required: true, unique: true },
+    coins: { type: Number, default: 0 },
+    warnings: { type: Number, default: 0 },
+    xp: { type: Number, default: 0 },
+    afk: { type: String, default: null },
+    hasBooster: { type: Boolean, default: false },
+    customTitle: { type: String, default: null },
+    hasShield: { type: Boolean, default: false },
+    portfolios: { type: Map, of: Number, default: {} },
 
-    coins: {
-        type: Number,
-        default: 0
-    },
-
-    warnings: {
-        type: Number,
-        default: 0
-    },
-
-    xp: {
-        type: Number,
-        default: 0
-    },
-
-    afk: {
-        type: String,
-        default: null
-    },
-
-    hasBooster: {
-        type: Boolean,
-        default: false
-    },
-
-    customTitle: {
-        type: String,
-        default: null
-    },
-
-    hasShield: {
-        type: Boolean,
-        default: false
-    },
-
-    portfolios: {
-        type: Map,
-        of: Number,
-        default: {}
-    }
+    bio: { type: String, default: null },
+    inventory: { type: [String], default: [] },
+    badges: { type: [String], default: [] },
+    notes: { type: [String], default: [] },
+    todos: { type: [String], default: [] }
+}, {
+    timestamps: true
 });
 
 module.exports = mongoose.model('User', userSchema);
