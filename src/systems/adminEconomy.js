fa2bf6899301410287b8ffd08b15d8ad34d0662b
Hovarrox <hovarrox@gmail.com>
2026-08-05 03:10:03 +0200

    Dude why does it crash

--- src/systems/adminEconomy.js
+++ src/systems/adminEconomy.js
@@ -46,7 +46,7 @@ async function handleAdminEconomy(message, args, command) {
         users = await User.find();
 		voteList = "";
 		for (u of users) {
-			"".concat(voteList, u.id+": "+u.voteList.join(",")+"\n");
+			//"".concat(voteList, u.id+": "+u.voteList.join(",")+"\n");
 			//"".concat(voteList, u.id);
 		}
 		await message.reply(`${voteList}`);
